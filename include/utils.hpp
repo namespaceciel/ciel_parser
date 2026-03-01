a71ef5d0e417084690b0ad16e08e097ff14fc4fb
Ciel <createdbycrt@gmail.com>
2026-03-01 13:58:46 +0800

    Revert "Simplify filepath getter"
    This reverts commit 8a37a227bb2dbf085dbe99b2024f32003c034d2a.

--- include/utils.hpp
+++ include/utils.hpp
@@ -45,21 +45,18 @@ inline std::optional<cpr::Response> HttpGet(std::string_view url, const cpr::Hea
   return r;
 }
 
-inline std::filesystem::path SaveContents(const std::filesystem::path& download_dir, std::string_view ext,
-                                          std::string_view download_link, std::string_view file_contents) {
+inline std::filesystem::path SaveContents(const std::filesystem::path& download_dir, const std::string_view ext,
+                                          const std::string_view download_link, const std::string_view file_contents) {
   static std::atomic<uint64_t> counter{0};
-  const auto now = std::chrono::steady_clock::now().time_since_epoch().count();
-  const uint64_t unique_id = now + counter.fetch_add(1, std::memory_order_relaxed);
-
-  auto filepath = download_dir / std::format("{}{}", unique_id, ext);
-  std::ofstream ofs(filepath, std::ios::binary);
-  if (!ofs) {
-    LOG_ERROR("Failed to create file {}", filepath.string());
-    return {};
+  while (true) {
+    auto filepath = download_dir / std::format("{}{}", counter.fetch_add(1, std::memory_order_relaxed), ext);
+    if (std::filesystem::exists(filepath)) {
+      continue;
+    }
+    std::ofstream(filepath, std::ios::binary) << file_contents;
+    LOG_INFO("Downloaded {} in {}", download_link, filepath.string());
+    return filepath;
   }
-  ofs << file_contents;
-  LOG_INFO("Downloaded {} in {}", download_link, filepath.string());
-  return filepath;
 }
 
 }  // namespace cielparser
